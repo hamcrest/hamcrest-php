0b8cacfa5ab45eb66acac62df16a8b4995fa21ab
Nils Luxton <ascii.soup@gmail.com>
2016-07-15 15:49:28 +0100

    Added a test for __invoke() on BaseMatcher

---
+++ tests/Hamcrest/InvokedMatcherTest.php
@@ -0,0 +1,32 @@
+<?php
+namespace Hamcrest;
+
+
+class SampleInvokeMatcher extends BaseMatcherTest
+{
+    private $matchAgainst;
+
+    public function __construct($matchAgainst)
+    {
+        $this->matchAgainst = $matchAgainst;
+    }
+
+    public function matches($item)
+    {
+        return $item == $this->matchAgainst;
+    }
+
+}
+
+class InvokedMatcherTest extends \PHPUnit_Framework_TestCase
+{
+    public function testInvokedMatchersCallMatches()
+    {
+        $sampleMatcher = new SampleInvokeMatcher("foo");
+
+        $this->assertEquals(
+            $sampleMatcher("foo"),
+            true
+        );
+    }
+}
