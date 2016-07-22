b7a5e18824117d8b65942e9aa77425d9b7dd7ff8
ASCII Soup <ascii.soup@gmail.com>
2016-07-22 15:03:17 +0100

    Matchers can now be used as callables (#40)
    Matchers can now be used as callables

---
+++ tests/Hamcrest/InvokedMatcherTest.php
@@ -0,0 +1,30 @@
+<?php
+namespace Hamcrest;
+
+
+class SampleInvokeMatcher extends BaseMatcherTest
+{
+    private $matchAgainst;
+
+    public function __construct($matchAgainst)
+    {
+        $this->matchAgainst = $matchAgainst;
+    }
+
+    public function matches($item)
+    {
+        return $item == $this->matchAgainst;
+    }
+
+}
+
+class InvokedMatcherTest extends \PHPUnit_Framework_TestCase
+{
+    public function testInvokedMatchersCallMatches()
+    {
+        $sampleMatcher = new SampleInvokeMatcher('foo');
+
+        $this->assertTrue($sampleMatcher('foo'));
+        $this->assertFalse($sampleMatcher('bar'));
+    }
+}
